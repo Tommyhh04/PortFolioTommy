d8fd8ecdfb0a5333ee703eb4ae0a7c64dc8b0995
Eggcanoe <tommyhalleland@hotmail.com>
2024-01-03 09:37:04 +0100

    Added some projects to data

--- Portfolio/src/Components/MyProjects/MyProjectsData.jsx
+++ Portfolio/src/Components/MyProjects/MyProjectsData.jsx
@@ -1,1 +1,28 @@
 //Contains data for the different projects to be displayed in MyProjects.jsx
+
+const projects = [
+  {
+    id: 1,
+    title: "Javascript Guitar player",
+    img: "GuitarPiano.png",
+    desc: "Playable guitar in browser",
+    url: "https://tommyhh04.github.io/Guitar-PianoPlayer/",
+  },
+  {
+    id: 2,
+    title: "Password Generator",
+    img: "PasswordGen.png",
+    alt: "Thumbnail for project",
+    desc: "A password generator",
+    url: "",
+  },
+  {
+    id: 3,
+    title: "Pokedex",
+    img: "Pokedex.png",
+    alt: "Thumbnail for project",
+    desc: "Pokedex with API and JavaScript",
+  },
+];
+
+export default projects;
